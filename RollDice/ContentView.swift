e64c70f378b4ded059dda69347b93fb3705b5ea0
chef-nic <nicholasajohnson@me.com>
2024-08-06 15:31:13 -0700

    Initial Commit

---
+++ RollDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RollDice
+//
+//  Created by Nicholas Johnson on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Nicholas Johnson on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
